70f2a75408602c7e200aa7cf752f61388e9db6c7
robohen <88636103+robohen@users.noreply.github.com>
2022-11-10 10:37:11 -0800

    backend complete time to work on frontend

--- routes/exercises.js
+++ routes/exercises.js
@@ -10,17 +10,49 @@ router.route('/').get((req, res) =>{
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const description = req.body.description;
-    const duration = Number(req.body.duration);
+    const setsAndReps = Number(req.body.setsAndReps);
     const date = Date.parse(req.body.date);
+    const extraNotes = req.body.extraNotes;
 
     const newExercise = new Exercise({
         username,
         description,
-        duration,
+        setsAndReps,
         date,
+        extraNotes,
     });
 
     newExercise.save()
     .then(() => res.json('Exercise added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+//object id returns the information of that exercise
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(exercise => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+router.route('/update/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.setsAndReps = req.body.setsAndReps;
+        exercise.date = Date.parse(req.body.date);
+        exercise.extraNotes = req.body.extraNotes;
+
+        exercise.save()
+        .then(()=> res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
